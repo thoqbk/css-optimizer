4eaf278e7bbd97ff67c36fd187fb7b387c72930b
Tho Q Luong <thoqbk@gmail.com>
2015-08-25 13:51:48 +0700

    Fix bug: path seperator in Test file

--- src/test/java/com/megaads/css/optimizer/test/TestCSSOptimizer.java
+++ src/test/java/com/megaads/css/optimizer/test/TestCSSOptimizer.java
@@ -52,26 +52,26 @@ public class TestCSSOptimizer {
 
         String projectDirectory = System.getProperty("user.dir");
         String resourceDirectory = projectDirectory
-                + "\\src\\main\\resources\\com\\megaads\\css\\optimizer\\resource";
+                + "/src/main/resources/com/megaads/css/optimizer/resource";
         //Initialize CSSOptimizer with target css file(file will be optimized) 
         //and output file (result file after optimizing target cssfile)
-        CSSOptimizer optimizer = new CSSOptimizer(resourceDirectory + "\\bootstrap.css",
-                resourceDirectory + "\\bootstrap.min.css");
+        CSSOptimizer optimizer = new CSSOptimizer(resourceDirectory + "/bootstrap.css",
+                resourceDirectory + "/bootstrap.min.css");
 
         //add sample files
-        optimizer.addHtmlFile(resourceDirectory + "\\about-us.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\airhead.com.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\amazon.com.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\apparel-amp-amp-accessories.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\blog.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\bodywear.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\categories.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\choosing-perfect-top-for-date-night.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\home.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\stores.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\modal.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\coupon-code-2015-find-coupons-discount-codes.htm");
-        optimizer.addHtmlFile(resourceDirectory + "\\up-to-70-off-electronics-outlet-free-shipping.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/about-us.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/airhead.com.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/amazon.com.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/apparel-amp-amp-accessories.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/blog.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/bodywear.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/categories.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/choosing-perfect-top-for-date-night.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/home.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/stores.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/modal.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/coupon-code-2015-find-coupons-discount-codes.htm");
+        optimizer.addHtmlFile(resourceDirectory + "/up-to-70-off-electronics-outlet-free-shipping.htm");
         optimizer.addHtmlFile("https://github.com/thoqbk");
 
         //some classes or tags are added using Javascript code, declare them here:
